4e596bf28fa1c7ca052b11b2bc5effab11237cbc
Pedro Dias <pedrodias2208@gmail.com>
2026-08-14 20:41:02 -0300

    Implement salary calculation and input handling
    Fix input format and calculation for minimum salaries.

---
+++ Ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  float smin=1621.00;
+  float sbruto;
+  float quantos_salarios;
+  printf("Seu salário bruto é?: ");
+  scanf("%d\n", sbruto);
+  quantos_salarios=sbruto/smin
+  printf("Você recebe %d salários mínimos!\n", quantos_salarios)
+  
+  return 0;
+  
+}
