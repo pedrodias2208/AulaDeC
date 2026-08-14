382e71dbeea0c2227c53d3c5dbfdc1edb60e5986
Pedro Dias <pedrodias2208@gmail.com>
2026-08-14 23:27:06 +0000

    ano de fundação mackFCI

--- exemplo1.c
+++ exemplo1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(){
-  printf("Hello World!\n");
+  printf("Mackenzie FCI\n");
+  printf("Algoritmos e Programção II\n");
+  printf("02N11\n");
+  printf("%d\n", 2026 - 55);
+
+
   return 0;
 }
