1b8c07fdfd2c9129d199cae28b3aaa25927d2552
Pedro Dias <pedrodias2208@gmail.com>
2026-08-18 21:52:49 -0300

    Correct time input and calculate minutes
    Fix input format specifier and calculation of minutes.

---
+++ Ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int hora;
+  int minutos;
+  printf("Digite a hora atual");
+  scanf("%f", & hora);
+  minutos=hora*60
+  printf("Se passaram %f minutos desde o começo do dia", minutos);
+  return 0;
+  
+
+}
