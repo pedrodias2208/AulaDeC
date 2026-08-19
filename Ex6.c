8f2d454c509653b8551919469fbec15b99c27efc
Pedro Dias <pedrodias2208@gmail.com>
2026-08-19 04:15:42 +0000

    Feito

--- Ex6.c
+++ Ex6.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main(){
-  
+  int numero;
+  printf("Indique um número para descobrir se é multiplo de 5: ");
+  scanf("%d", &numero);
+  if (numero%5==0){
+    printf("O número é multiplo de 5\n");
+
+  } 
+  else{
+    printf("O número não é múltiplo de 5\n");
+  }
+ return 0;
 }
