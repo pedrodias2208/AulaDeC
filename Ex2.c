32d66c41c1762d52b5860ddc774bdd10578333b0
Pedro Dias <pedrodias2208@gmail.com>
2026-08-18 22:04:02 -0300

    Fix input format and output for minutes calculation

--- Ex2.c
+++ Ex2.c
@@ -3,9 +3,9 @@ int main(){
   int hora;
   int minutos;
   printf("Digite a hora atual");
-  scanf("%f", & hora);
-  minutos=hora*60
-  printf("Se passaram %f minutos desde o começo do dia", minutos);
+  scanf("%d", & hora);
+  minutos=hora*60;
+  printf("Se passaram %d minutos desde o começo do dia", minutos);
   return 0;
   
 
