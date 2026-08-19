08a3acbbc3444ac665e39f2a4ad650a20ca73e91
Pedro Dias <pedrodias2208@gmail.com>
2026-08-19 04:22:07 +0000

    arrumei

--- Ex2.c
+++ Ex2.c
@@ -2,10 +2,10 @@
 int main(){
   int hora;
   int minutos;
-  printf("Digite a hora atual");
+  printf("Digite a hora atual: \n");
   scanf("%d", & hora);
   minutos=hora*60;
-  printf("Se passaram %d minutos desde o começo do dia", minutos);
+  printf("Se passaram %d minutos desde o começo do dia\n", minutos);
   return 0;
   
 
