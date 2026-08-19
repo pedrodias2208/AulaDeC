b7d97520e4d1f92f8e0c00c4e71712775ba8b34b
Pedro Dias <pedrodias2208@gmail.com>
2026-08-19 01:44:33 +0000

    Ta certo

--- Ex4.c
+++ Ex4.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 int main(){
-  
+  float base;
+  float altura;
+  float area;
+  printf("Indique a base: \n");
+  scanf("%f", &base);
+  printf("Indique a altura: \n");
+  scanf("%f", &altura);
+  area=base*altura;
+  printf("A área é: %.2f\n", area);
+  return 0;
 }
