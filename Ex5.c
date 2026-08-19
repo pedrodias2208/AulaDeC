6b9c1147a6768d12c1b8c2770d37712cb0683395
Pedro Dias <pedrodias2208@gmail.com>
2026-08-19 04:00:34 +0000

    fiz certo

--- Ex5.c
+++ Ex5.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 int main(){
-  
+  float salario_minimo;
+  float dolar;
+  float quant_casas;
+  float real;
+
+  printf("Indique o valor do salário mínimo: \n");
+  scanf("%f", &salario_minimo);
+  printf("Indique o valor do Dólar hoje: \n");
+  scanf("%f", &dolar);
+  real=10000000*dolar;
+  quant_casas=real/(150*salario_minimo);
+  printf("É possível construir %.2f casas\n", quant_casas);
+  return 0;
+
+
 }
