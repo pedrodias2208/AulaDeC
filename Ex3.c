aeca43df0b79488f628ebd5a988c3204595ba8c1
Pedro Dias <pedrodias2208@gmail.com>
2026-08-19 01:27:25 +0000

    arrumado 2

--- Ex3.c
+++ Ex3.c
@@ -6,12 +6,12 @@ int main(){
   float media;
   float soma;
   
-  printf("Insira a NOTA 1: ");
-  scanf("%f\n", &nota1);
+  printf("NOTA 1: ");
+  scanf("%f", &nota1);
   printf("NOTA 2: ");
-  scanf("%f\n", &nota2);
+  scanf("%f", &nota2);
   printf("NOTA 3: ");
-  scanf("%f\n", &nota3);
+  scanf("%f", &nota3);
   soma=nota1+nota2+nota3;
   media=soma/3;
   printf("A média é: %f\n", media);
